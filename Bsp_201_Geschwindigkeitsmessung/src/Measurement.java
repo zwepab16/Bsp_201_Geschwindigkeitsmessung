e05009646cd97e81f8e3e6c124282677502fcdb4
zwepab16 <zwepab16@htlkaindorf.at>
2018-10-03 12:47:00 +0200

    Messungs Klasse vervollständigt

--- Bsp_201_Geschwindigkeitsmessung/src/Measurement.java
+++ Bsp_201_Geschwindigkeitsmessung/src/Measurement.java
@@ -1,5 +1,33 @@
 
+import java.time.LocalDateTime;
+
+
 
 public class Measurement {
+private LocalDateTime datum;
+private String kennzeichen;
+private int gemessen,erlaubt;
 
+    public LocalDateTime getDatum() {
+        return datum;
+    }
+
+    public String getKennzeichen() {
+        return kennzeichen;
+    }
+
+    public int getGemessen() {
+        return gemessen;
+    }
+
+    public int getErlaubt() {
+        return erlaubt;
+    }
+
+    public Measurement(LocalDateTime datum, String kennzeichen, int gemessen, int erlaubt) {
+        this.datum = datum;
+        this.kennzeichen = kennzeichen;
+        this.gemessen = gemessen;
+        this.erlaubt = erlaubt;
+    }
 }
