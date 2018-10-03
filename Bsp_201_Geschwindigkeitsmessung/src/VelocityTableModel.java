5bd9833faa424287c3fbcf23b899cf2303bb6ad3
zwepab16 <zwepab16@htlkaindorf.at>
2018-10-03 12:24:21 +0200

    Klassen erstellt

---
+++ Bsp_201_Geschwindigkeitsmessung/src/VelocityTableModel.java
@@ -0,0 +1,23 @@
+
+import javax.swing.table.AbstractTableModel;
+
+
+
+public class VelocityTableModel extends AbstractTableModel{
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
